8619c177541b387b6bff8230871e981cb9ef21fc
Daniel-Allum <daniel.allum@education.nsw.gov.au>
2025-07-30 23:50:23 +0000

    start

--- The humble adventures of Garbanzo/main.py
+++ The humble adventures of Garbanzo/main.py
@@ -1,7 +1,9 @@
-print("You awaken in a dimly-lit chamber, the sound of dripping water echoing around you.")
+from room import Room
+
+print("You awaken in a dark gloomy chamber, the sound of dripping water echoing around you.")
 choice = input("Do you (1) Light your torch, or (2) Feel your way forward in the dark? ")
 
 if choice == "1":
-    print("The torch flickers to life. Shadows dance across ancient walls.")
+    print("\nYou strike your matches, the torch flickers to life. Shadows dance across ancient walls.")
 elif choice == "2":
-    print("You stumble forward blindly... and trip over something soft.")
+    print("\nYou stumble forward blindly... and trip over something soft.")
