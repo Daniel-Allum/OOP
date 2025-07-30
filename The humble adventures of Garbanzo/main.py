e84b107fa408dfaad4c8f141516f65dc855fe87d
Daniel-Allum <daniel.allum@education.nsw.gov.au>
2025-07-30 04:08:40 +0000

    start of my game

---
+++ The humble adventures of Garbanzo/main.py
@@ -0,0 +1,7 @@
+print("You awaken in a dimly-lit chamber, the sound of dripping water echoing around you.")
+choice = input("Do you (1) Light your torch, or (2) Feel your way forward in the dark? ")
+
+if choice == "1":
+    print("The torch flickers to life. Shadows dance across ancient walls.")
+elif choice == "2":
+    print("You stumble forward blindly... and trip over something soft.")
